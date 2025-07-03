ae2b97b1f74c573b535d8920462d123762df7bba
Sharnam <sharnam34@gmail.com>
2025-07-03 16:44:07 +0700

    bugfix: Config fixes

--- src/pages/services.jsx
+++ src/pages/services.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useRef } from "react";
-import { useScroll, motion, useAnimation } from "framer-motion";
+import { motion, useAnimation } from "framer-motion";
 import { useInView } from "react-intersection-observer";
 import { services } from "../data/services";
 import { useLocation } from "react-router-dom";
